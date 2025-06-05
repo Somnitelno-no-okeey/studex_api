9b14e63e8580a5868a738d98ce92c6f7cde6eeec
Dacst12 <misiya3@yandex.ru>
2025-06-05 22:46:51 +0500

    update reviews and discipline models and add signals for reviews

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -4,20 +4,7 @@ from apps.common.models import IsDeletedModel
 from apps.disciplines.models import Discipline
 
 
-RATING_CHOICES = (
-        (0, '0'),
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
-        (6, '6'),
-        (7, '7'),
-        (8, '8'),
-        (9, '9'),
-        (10, '10')
-    )
-
+RATING_CHOICES = [(i, str(i)) for i in range(6)]
 
 
 class Review(IsDeletedModel):
@@ -25,56 +12,20 @@ class Review(IsDeletedModel):
     discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE, related_name='reviews')
     review_text = models.TextField(verbose_name='Текст отзыва')
     is_anonymous = models.BooleanField(default=False, verbose_name='Анонимность')
-    ai_evaluation = models.IntegerField(verbose_name='Оценка отзыва нейросетью')
 
-    avg_interest = models.FloatField(
-        default=None,
-        verbose_name="Интересность дисциплины"
-    )
-    avg_complexity = models.FloatField(
-        default=None,
-        verbose_name="Уровень сложности"
-    )
-    avg_usefulness = models.FloatField(
-        default=None,
-        verbose_name="Полезность содержания"
-    )
-    avg_workload = models.FloatField(
-        default=None,
-        verbose_name="Объем нагрузки"
-    )
-    avg_logical_structure = models.FloatField(
-        default=None,
-        verbose_name="Логичность структуры"
-    )
-    avg_practical_applicability = models.FloatField(
-        default=None,
-        verbose_name="Практическая применимость"
-    )
-    avg_teaching_effectiveness = models.FloatField(
-        default=None,
-        verbose_name="Эффективность преподавания"
-    )
-    avg_materials_availability = models.FloatField(
-        default=None,
-        verbose_name="Доступность учебных материалов"
-    )
-    avg_feedback_support = models.FloatField(
-        default=None,
-        verbose_name="Обратная связь и поддержка"
-    )
+    avg_interest = models.FloatField(null=True, verbose_name="Интересность дисциплины")
+    avg_complexity = models.FloatField(null=True, verbose_name="Уровень сложности")
+    avg_usefulness = models.FloatField(null=True, verbose_name="Полезность содержания")
+    avg_workload = models.FloatField(null=True, verbose_name="Объем нагрузки")
+    avg_logical_structure = models.FloatField(null=True, verbose_name="Логичность структуры")
+    avg_practical_applicability = models.FloatField(null=True, verbose_name="Практическая применимость")
+    avg_teaching_effectiveness = models.FloatField(null=True, verbose_name="Эффективность преподавания")
+    avg_materials_availability = models.FloatField(null=True, verbose_name="Доступность учебных материалов")
+    avg_feedback_support = models.FloatField(null=True, verbose_name="Обратная связь и поддержка")
 
     class Meta:
         verbose_name = "Отзыв"
         verbose_name_plural = "Отзывы"
 
-
-
-class Comment(IsDeletedModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
-    comment_text = models.TextField(verbose_name='Текст комментария')
-
-    class Meta:
-        verbose_name = "Комментарий"
-        verbose_name_plural = "Комментарии"
+    def __str__(self):
+        return f"Отзыв от {self.user} для {self.discipline}"
